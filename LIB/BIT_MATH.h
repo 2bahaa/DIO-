1ad5b3b27e79f8168a6ef05d8a3626d02a8ba0d3
mohamed bahaa <159546293+2bahaa@users.noreply.github.com>
2024-05-07 21:25:45 +0300

    Add files via upload

---
+++ LIB/BIT_MATH.h
@@ -0,0 +1,15 @@
+/*
+ * BIT_MATH.h
+ *
+ * Created: 5/5/2024 11:43:22 PM
+ *  Author: bahaa and bahram
+ */ 
+
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#define	 SET_BIT(reg,bit)	reg|=(1<<bit)               //Text replacement to SET a special bit in a specific register
+#define	 CLR_BIT(reg,bit)	reg&=(~(1<<bit))            //Text replacement to Clear a special bit in a specific register
+
+#endif /* BIT_MATH_H_ */
---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,14 @@
+/*
+ * STD_TYPES.h
+ *
+ * Created: 5/5/2024 11:42:30 PM
+ *  Author: bahram and bahaa
+ */ 
+
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char  uint8;     // define an integer with only 8 bit not 32 bit 
+
+#endif /* STD_TYPES_H_ */
